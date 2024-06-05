57d166cc1808341fb7258b7d438c0782de07600c
NagendraGowda <90629482+NagendraGowda@users.noreply.github.com>
2024-06-06 01:30:01 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpHhlDJK7_7DPl7ANyyjIh6acz_31x7EM",
-  authDomain: "mybuddy-fad6e.firebaseapp.com",
-  projectId: "mybuddy-fad6e",
-  storageBucket: "mybuddy-fad6e.appspot.com",
-  messagingSenderId: "621522680623",
-  appId: "1:621522680623:web:e00c410802e2947da0ffda",
-  measurementId: "G-11M0P90RSY",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
